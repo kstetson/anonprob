776c649f3a3f788588ca4d3f9fc538abe40b0789
thebenedict <thebenedict@gmail.com>
2016-07-12 17:04:39 +0300

    add a tree background, roughly position the content area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,3 +56,38 @@ var geo_options = {
 };
 
 var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+
+// positioning
+
+var image = { width: 3300, height: 2550 };
+var target = { x: 1600, y: 1850 };
+
+var wrapper = $('.content-wrapper');
+
+$(document).ready(updatePointer);
+$(window).resize(updatePointer);
+
+function updatePointer() {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    
+    // Get largest dimension increase
+    var xScale = windowWidth / image.width;
+    var yScale = windowHeight / image.height;
+    var scale;
+    var yOffset = 0;
+    var xOffset = 0;
+    
+    if (xScale > yScale) {
+        // The image fits perfectly in x axis, stretched in y
+        scale = xScale;
+        yOffset = (windowHeight - (image.height * scale)) / 2;
+    } else {
+        // The image fits perfectly in y axis, stretched in x
+        scale = yScale;
+        xOffset = (windowWidth - (image.width * scale)) / 2;
+    }
+    
+    wrapper.css('top', (target.y) * scale + yOffset);
+    wrapper.css('left', (target.x) * scale + xOffset);
+}
